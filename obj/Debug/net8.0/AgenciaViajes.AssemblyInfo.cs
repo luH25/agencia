e3d9a0a6fcd91f7f2a3ce636f4076a9355fa3444
JesusMamani72 <jdmb724@gmail.com>
2024-07-02 14:47:05 -0400

    agregacion, modificacion y decoracion de recibos

--- obj/Debug/net8.0/AgenciaViajes.AssemblyInfo.cs
+++ obj/Debug/net8.0/AgenciaViajes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgenciaViajes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cedaf3a027f227c8a5ebb390ba9600e2aeaf24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cccf4cfeecaeb82c1b86333216593531c5a8ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgenciaViajes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgenciaViajes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
